6723d4fc988f34d8677253c571136c35d09a8ffb
Ivan Grokhotkov <ivan@espressif.com>
2021-12-13 22:05:55 +0100

    tlsf_public and tlsf_private moved to tlsf_config enum in tlsf_common.h header

--- tlsf_common.h
+++ tlsf_common.h
@@ -6,9 +6,52 @@
 
 #pragma once
 
+#if defined __has_include && __has_include("tlsf_platform.h")
+#include "tlsf_platform.h"
+#endif
+
 #if defined(__cplusplus)
 extern "C" {
 #endif
+
+enum tlsf_config
+{
+	/* log2 of number of linear subdivisions of block sizes. Larger
+	** values require more memory in the control structure. Values of
+	** 4 or 5 are typical.
+	*/
+	SL_INDEX_COUNT_LOG2  = 5,
+
+	/* All allocation sizes and addresses are aligned to 4 bytes. */
+	ALIGN_SIZE_LOG2 = 2,
+	ALIGN_SIZE = (1 << ALIGN_SIZE_LOG2),
+
+	/*
+	** We support allocations of sizes up to (1 << FL_INDEX_MAX) bits.
+	** However, because we linearly subdivide the second-level lists, and
+	** our minimum size granularity is 4 bytes, it doesn't make sense to
+	** create first-level lists for sizes smaller than SL_INDEX_COUNT * 4,
+	** or (1 << (SL_INDEX_COUNT_LOG2 + 2)) bytes, as there we will be
+	** trying to split size ranges into more slots than we have available.
+	** Instead, we calculate the minimum threshold size, and place all
+	** blocks below that size into the 0th first-level list.
+	*/
+
+	/* Tunning the first level, we can reduce TLSF pool overhead
+	 * in exchange of manage a pool smaller than 4GB
+	 */
+	#ifdef FL_INDEX_MAX_PLATFORM
+	FL_INDEX_MAX = FL_INDEX_MAX_PLATFORM,
+	#else
+	FL_INDEX_MAX = 30,
+	#endif
+
+	SL_INDEX_COUNT = (1 << SL_INDEX_COUNT_LOG2),
+	FL_INDEX_SHIFT = (SL_INDEX_COUNT_LOG2 + ALIGN_SIZE_LOG2),
+	FL_INDEX_COUNT = (FL_INDEX_MAX - FL_INDEX_SHIFT + 1),
+
+	SMALL_BLOCK_SIZE = (1 << FL_INDEX_SHIFT),
+};
 
 #if defined(__cplusplus)
 };
